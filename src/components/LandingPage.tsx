f75d4b3b460705835041821926d72bc821fb6813
Kibiwott <bk.kibiwott@gmail.com>
2025-04-12 09:48:29 +0300

    Added the landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,46 +1,44 @@
-import { Box, Button, Container, Stack, Typography } from "@mui/material"
-import {Link} from 'react-router-dom'
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { Box, Button, Grid, Stack, Typography } from '@mui/material'
+import EastIcon from '@mui/icons-material/East';
+import Cover from "../assets/images/blog-examples-1.jpg"
+
 
 const LandingPage = () => {
   return (
-    <Box maxWidth={"xl"}  bgcolor="seagreen" sx={{marginInline: 0}}>
-        <Container   >
-            <Stack direction={{xs:"column"}}  textAlign="center">
-               <Box>    
-                <Typography variant="h3" fontWeight="bold" gutterBottom>
-                    Share Your Story with the World
+    <Box maxWidth="xl" flexDirection={ {lg:"row", sm:"column", xs:"column"}   } gap={2} display={"flex"}   sx={{mt:12,mb:4,mx:2}}>
+        <Stack direction="column" >
+            <Box >
+                <Typography variant='h2' component="h4" gutterBottom >
+                    Write your Story to the World
                 </Typography>
-                <Typography variant="body1" paragraph>
-                    Join a vibrant community of readers and writers. Publish your thoughts, experiences,
-                    and insights on a platform designed for storytellers
+                <Typography>
+                    Empower your voice, connect with readers, and join a global community of storytellers. Whether  you're a seasoned writer or just starting out, our platform gives your words the spotlight they deserve.
                 </Typography>
-               </Box>
-               <Stack sx={{mt:{xs:4}, gap:3 ,px:2}} direction={"row"} >
-                <Button 
-                variant="contained"
-                
-                component={Link}
-                to="/register" 
-                endIcon={<ArrowForwardIcon/> }                      
-                >
+              
+               
+            </Box>
+           <Grid container justifyContent={"flex-start"} gap={2} mt={3}>
+            <Grid>
+                <Button variant='contained' endIcon={<EastIcon />}>
                     Start Writing
                 </Button>
-                <Button 
-                variant="outlined" 
-                component={Link} 
-                to="/register"
-                >
-                    Explore stories
+            </Grid>
+            <Grid>
+                <Button variant='outlined'>
+                Explore
                 </Button>
-               </Stack>
-            </Stack>
-            <Box>
-                
-            </Box>
-
-        </Container>
-      
+            </Grid>
+           </Grid>
+        </Stack>
+        <Box
+        maxWidth={"40rem"}
+        component={"img"}
+        src={Cover}
+        width="100%"
+        sx={{mt:3,b:3}}
+        
+        />
+        
     </Box>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,24 @@
-import { Box, Container, Typography } from '@mui/material'
+import { Box, Container, Stack, Typography } from '@mui/material'
 
 import LandingPage from '../components/LandingPage'
+import './styles/landingpage.css'
 
 
 const Home = () => {
   return (
-  <Box sx={{mt:12}}>
-    <LandingPage />
+  <Box sx={{
+    width: "100%",
+    overflow:"hidden",
+    right:0,
+    left:0,
+    position:"absolute",
+    padding:0,
+    margin:0,     
+    
+  }} >
+    <Box sx={{padding:{sm:2, xs:2}}}>
+      <LandingPage />
+    </Box>
   </Box>
   )
 }
