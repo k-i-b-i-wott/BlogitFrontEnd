7af24787e1208edb2c4ebb7960d75c53ac327c47
Kibiwott <bk.kibiwott@gmail.com>
2025-04-13 15:11:57 +0300

    Added the view blog post page

--- src/pages/BlogEntry.tsx
+++ src/pages/BlogEntry.tsx
@@ -1,15 +1,21 @@
-import { Box, CircularProgress, Typography } from '@mui/material'
+import { Box, CircularProgress, Typography,Button } from '@mui/material'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import ReactMarkdown from 'react-markdown'
 import {  useParams } from 'react-router-dom'
+import{useEffect, useState} from 'react'
+
+import { Link } from 'react-router-dom'
+
 
 const BlogEntry = () => {
 
   const{blogId}=useParams()
+
+  const [blogError, setBlogError] = useState<string | null>(null);
   
 
-    const {isLoading,data}= useQuery({
+    const {isLoading,data, isError,error}= useQuery({
       queryKey:['Fetching-entry'],
       queryFn:async()=>{
         const response = await axios.get(`http://localhost:3000/blog/post/${blogId}`,
@@ -20,6 +26,29 @@ const BlogEntry = () => {
         
       }
     })   
+   useEffect(()=>{
+    if(isError){
+      if(axios.isAxiosError(error)){
+        const errorData = error.response?.data.message
+        setBlogError(errorData);
+      }else{
+        setBlogError("Blog not found")
+      }
+    }
+   },[error])
+    if(blogError){
+      return(
+        <Box sx={{display:"flex",flexDirection:"column",gap:3}}>
+              <Typography variant='h2'component={'h1' } sx={{textAlign:"center",mt:12}}>
+                {blogError}                
+            </Typography>
+            <Button variant='contained' component={Link} to="/writeblogs">  
+               Start Writing
+          </Button>
+        </Box>
+      )
+      
+    }
 
     if(isLoading){
       return <CircularProgress/>
--- src/pages/MyBlogs.tsx
+++ src/pages/MyBlogs.tsx
@@ -1,37 +1,96 @@
 import { Box, Button, Card, CardActions, CardContent, Grid, IconButton, Typography } from '@mui/material'
 import { GoArrowUpRight } from "react-icons/go";
 
+import {useQuery} from "@tanstack/react-query"
+import axios from 'axios'
+import {format} from 'date-fns'
+
 import{ Link} from 'react-router-dom'
 const MyBlogs = () => {
-  return (
-    <Box sx={{mt:12, display:"flex", justifyContent:"center", flexDirection:"column"}}>
-        <Card sx={{maxWidth:"md", maxHeight:"sm"}}>
-          <CardContent sx={{display:"flex", flexDirection:"column",gap:3, px:2}}>
+
+  
+
+const {isLoading,data,isError,error} =useQuery({
+    queryKey:["myBlogs"],
+    queryFn: async () => {
+      const response = await axios.get(`http://localhost:3000/blog/post`,{withCredentials:true})
+      console.log(response.data)
+      return response.data.data
+    }
+  })
+  if(isLoading){
+    <Typography variant='h1' sx={{mt:12}}>
+      Loading ..... please wait
+    </Typography>
+  }
+  if(isError){
+    if(axios.isAxiosError(error)){
+      const errorData = error.response?.data.message
+      return <Typography variant='h1' sx={{mt:12}}>
+        {errorData}
+      </Typography>
+    }else{
+      return <Typography variant='h1' sx={{mt:12}}>
+        An error occured
+      </Typography>
+    }
+  }
+   
+
+
+return (
+    <Box sx={{mt:12, display:"flex", gap:3, justifyContent:"center", flexDirection:"column"}}>
+        {
+         data && data.map((blog:any)=>{
+            return(
+        <Card key={blog.blogId} sx={{maxWidth:"md", height:"fit-content",  mt:.5}}>
+          <CardContent sx={{display:"flex", flexDirection:"column",gap:2}}>
             <Grid container sx={{display:"flex", flexDirection:"column", gap:1}}>
               <Grid sx={{justifyContent:"center", display:"flex", flexDirection:"column", alignItems:"center"}}>
-                <Typography variant='h4' >Title blog</Typography>
+                <Typography variant='h4' >{blog.blogTitle}</Typography>
               </Grid>
-              <Grid sx={{display:"flex", flexDirection:"column"}}>
-                <Typography variant='body1'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, quae!</Typography>
+              <Grid sx={{display:"flex", flexDirection:"column",paddingLeft:4 }}>
+                <Typography variant='body1'> {blog.blogExcerpt}</Typography>
               </Grid>
+              
               <CardActions sx={{display:"flex", justifyContent:"flex-end"}}>
               
-            </CardActions>              
+            </CardActions>  
+                        
             </Grid>
-           
+            <Box display={"flex"} flexDirection={{"md":"row","lg":"row","sm":"column","xs":"column"}} sx={{justifyContent:"space-between"}}>
+              <CardContent sx={{display:"flex", justifyContent:"flex-start"}}>
+                  <Grid container>
+                    <Grid sx={{color:"error.main"}}>
+                      Created on:
+                    </Grid>
+                    <Grid sx={{ml:1, fontWeight:"bold"}}>
+                        {
+                          format(new Date(blog.createdAt), "do MMM yy")
+                        }
+                    </Grid>
+                   
+                  </Grid>             
+              </CardContent>
+
             <CardActions sx={{display:"flex", justifyContent:"flex-end", gap:3}}>
-            <Button component={Link} to="/myblogs" endIcon={<GoArrowUpRight /> }  sx={{color:"primary.main"}}>
-                More
-              </Button>
+               <Button component={Link} to={`/blog/${blog.blogId}`} endIcon={<GoArrowUpRight /> }  sx={{color:"primary.main"}}>
+                  More
+                </Button>
               <Button variant='contained' sx={{bgcolor:"primary.main"}}>
                 Update
               </Button>
               <Button variant='contained' sx={{bgcolor:"error.main"}}>
                 Delete
               </Button>
+              
             </CardActions>
+            </Box>
          </CardContent>        
         </Card>  
+            )
+          })
+        }
     </Box>
   )
 }
